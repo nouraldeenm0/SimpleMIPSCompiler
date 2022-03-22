7d58c17f294a813ddf7b35aa3bdec684c1a68b89
nouraldeenm0 <81125079+nouraldeenm0@users.noreply.github.com>
2022-03-22 11:44:43 +0200

    Partial Changes for removing Extra lines in the code
    for example if the code is:
        doSomething();
    
       doOtherThing();
    
    will change to:
       doSomething();
       doOtherThing

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
-	public static void main(String[] args) {
-
-	}
+    
+    public static void main(String[] args) {
+        StringBuilder asm = new StringBuilder(
+            "this is the first sentence\n\n"+
+            "this is the second sentence" 
+        );   
+        
+        System.out.println(asm);
+    }
+    
+    
+    public static void removeExtraLines(StringBuilder sb) {
+        boolean foundALineBreak = false;
+        for (int i = 0; i < sb.length(); ++i) {
+            foundALineBreak = (sb.charAt(i) == '\n');
+        }
+    }
 }
